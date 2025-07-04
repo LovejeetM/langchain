2cdf071490be6b59c7efd4d9dce355bff0363732
LovejeetM <lovejeetmatharu54@gmail.com>
2025-07-04 17:28:17 +0530

    airline example

--- airline_service_format.py
+++ airline_service_format.py
@@ -21,4 +21,33 @@ def get_flight_info(d: dict) -> str:
     """
     Example of a retrieval function which takes a dictionary as key. Resembles SQL DB Query
     """
-    req_keys= ["first_name", "last_name", "confirmation"]
+    req_keys = ['first_name', 'last_name', 'confirmation']
+    assert all((key in d) for key in req_keys), f"Expected dictionary with keys {req_keys}, got {d}"
+
+    keys = req_keys + ["departure", "destination", "departure_time", "arrival_time", "flight_day"]
+    values = [
+        ["Jane", "Doe", 12345, "San Jose", "New Orleans", "12:30 PM", "9:30 PM", "tomorrow"],
+        ["John", "Smith", 54321, "New York", "Los Angeles", "8:00 AM", "11:00 AM", "Sunday"],
+        ["Alice", "Johnson", 98765, "Chicago", "Miami", "7:00 PM", "11:00 PM", "next week"],
+        ["Bob", "Brown", 56789, "Dallas", "Seattle", "1:00 PM", "4:00 PM", "yesterday"],
+    ]
+    get_key = lambda d: "|".join([d['first_name'], d['last_name'], str(d['confirmation'])])
+    get_val = lambda l: {k:v for k,v in zip(keys, l)}
+    db = {get_key(get_val(entry)) : get_val(entry) for entry in values}
+
+    
+    data = db.get(get_key(d))
+    if not data:
+        return (
+            f"Based on {req_keys} = {get_key(d)}) from your knowledge base, no info on the user flight was found."
+            " This process happens every time new info is learned. If it's important, ask them to confirm this info."
+        )
+    return (
+        f"{data['first_name']} {data['last_name']}'s flight from {data['departure']} to {data['destination']}"
+        f" departs at {data['departure_time']} {data['flight_day']} and lands at {data['arrival_time']}."
+    )
+
+#######################################################################################
+print(get_flight_info({"first_name" : "Jane", "last_name" : "Doe", "confirmation" : 12345}))
+print(get_flight_info({"first_name" : "Alice", "last_name" : "Johnson", "confirmation" : 98765}))
+print(get_flight_info({"first_name" : "Bob", "last_name" : "Brown", "confirmation" : 27494}))
