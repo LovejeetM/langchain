5d0a3de3380c07f8ace4c8d343cb36d7e1efb8f9
LovejeetM <lovejeetmatharu54@gmail.com>
2025-07-26 21:45:51 +0530

    working with documents

--- working_with_documents.py
+++ working_with_documents.py
@@ -7,6 +7,21 @@ from langchain_nvidia_ai_endpoints import ChatNVIDIA
 ChatNVIDIA.get_available_models()
 from langchain_core.runnables import RunnableLambda
 from functools import partial
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.document_loaders import ArxivLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables.passthrough import RunnableAssign
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.output_parsers import PydanticOutputParser
+
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+
+from pydantic import BaseModel, Field
+from typing import List
+from IPython.display import clear_output
+
 
 console = Console()
 base_style = Style(color="#76B900", bold=True)
@@ -23,4 +38,49 @@ def PPrint(preface="State: "):
         pprint(preface, x)
         return x
     return RunnableLambda(partial(print_and_return, preface=preface))
+
+documents = ArxivLoader(query="2404.16130").load() 
+
+print("Number of Documents Retrieved:", len(documents))
+print(f"Sample of Document 1 Content (Total Length: {len(documents[0].page_content)}):")
+print(documents[0].page_content[:1000])
+
+pprint(documents[0].metadata)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1200,
+    chunk_overlap=100,
+    separators=["\n\n", "\n", ".", ";", ",", " ", ""],
+)
+
+## Some nice custom preprocessing
+# documents[0].page_content = documents[0].page_content.replace(". .", "")
+docs_split = text_splitter.split_documents(documents)
+
+print(len(docs_split))
+
+for i in (0, 1, 2, 15, -1):
+    pprint(f"[Document {i}]")
+    print(docs_split[i].page_content)
+    pprint("="*64)
+
+
+class DocumentSummaryBase(BaseModel):
+    running_summary: str = Field("", description="Running description of the document. Do not override; only update!")
+    main_ideas: List[str] = Field([], description="Most important information from the document (max 3)")
+    loose_ends: List[str] = Field([], description="Open questions that would be good to incorporate into summary, but that are yet unknown (max 3)")
+
+
+summary_prompt = ChatPromptTemplate.from_template(
+    "You are generating a running summary of the document. Make it readable by a technical user."
+    " After this, the old knowledge base will be replaced by the new one. Make sure a reader can still understand everything."
+    " Keep it short, but as dense and useful as possible! The information should flow from chunk to (loose ends or main ideas) to running_summary."
+    " The updated knowledge base keep all of the information from running_summary here: {info_base}."
+    "\n\n{format_instructions}. Follow the format precisely, including quotations and commas"
+    "\n\nWithout losing any of the info, update the knowledge base with the following: {input}"
+)
+
+
+
+
 
