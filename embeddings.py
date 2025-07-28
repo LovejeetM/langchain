814f34059c07d457b34234d56be22060bc18ae8e
LovejeetM <lovejeetmatharu54@gmail.com>
2025-07-28 23:12:32 +0530

    embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,6 +1,13 @@
 import os
 from dotenv import load_dotenv
 from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from operator import itemgetter
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
 
 load_dotenv()
 os.environ.get("NVIDIA_API_KEY")
@@ -17,3 +24,54 @@ embedder = NVIDIAEmbeddings(model="nvidia/nv-embed-v1")
 
 # ChatNVIDIA.get_available_models()
 instruct_llm = ChatNVIDIA(model="mistralai/mixtral-8x7b-instruct-v0.1")
+
+
+queries = [
+    "What's the weather like in Rocky Mountains?",
+    "What kinds of food is Italy known for?",
+    "What's my name? I bet you don't remember...",
+    "What's the point of life anyways?",
+    "The point of life is to have fun :D"
+]
+
+documents = [
+    "Komchatka's weather is cold, with long, severe winters.",
+    "Italy is famous for pasta, pizza, gelato, and espresso.",
+    "I can't recall personal names, only provide information.",
+    "Life's purpose varies, often seen as personal fulfillment.",
+    "Enjoying life's moments is indeed a wonderful approach.",
+]
+
+# Embedding the queries
+q_embeddings = [embedder.embed_query(query) for query in queries]
+
+# Embedding the documents
+d_embeddings = embedder.embed_documents(documents)
+
+def plot_cross_similarity_matrix(emb1, emb2):
+    # Compute the similarity matrix between embeddings1 and embeddings2
+    cross_similarity_matrix = cosine_similarity(np.array(emb1), np.array(emb2))
+
+    # Plotting the cross-similarity matrix
+    plt.imshow(cross_similarity_matrix, cmap='Greens', interpolation='nearest')
+    plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.title("Cross-Similarity Matrix")
+    plt.grid(True)
+
+plt.figure(figsize=(8, 6))
+plot_cross_similarity_matrix(q_embeddings, d_embeddings)
+plt.xlabel("Query Embeddings")
+plt.ylabel("Document Embeddings")
+plt.show()
+
+
+plt.figure(figsize=(8, 6))
+plot_cross_similarity_matrix(
+    q_embeddings,
+    [embedder.embed_query(doc) for doc in documents]
+)
+plt.xlabel("Query Embeddings (of queries)")
+plt.ylabel("Query Embeddings (of documents)")
+plt.show()
+
